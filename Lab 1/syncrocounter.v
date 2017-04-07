34d12752bcc4439b975b9db2deb19476457eaea7
Ahmed Moalim <ahmedm23@uw.edu>
2017-04-07 15:59:00 -0700

    Edited Bug in syncounter

--- Lab 1/syncrocounter.v
+++ Lab 1/syncrocounter.v
@@ -1,7 +1,7 @@
 // A four stage (4 bit) synchronous up counter, with active low reset.
 	//a. Design the counter using a dataflow model.
 	//b. Use the D flip-flop model in the adjacent listing.
-synchrocounter module(in, clk, reset, count);
+module synchrocounter (in, clk, reset, count);
 	input clk, reset;
 	input in [3:0];
 	output reg [3:0];
