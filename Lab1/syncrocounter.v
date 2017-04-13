3c022b67c277fcb742cc2cd6d5234d035acfb671
radleigh <radleigh@msn.com>
2017-04-12 20:42:45 -0700

    Fixed syncrocounter, (removed the reg), edited schementry

--- Lab1/syncrocounter.v
+++ Lab1/syncrocounter.v
@@ -4,22 +4,17 @@
 module synchrocounter (clk, reset, next);
 	input clk, reset;
 	wire [3:0] curr;
-	output reg [3:0] next;
-	
-	DFlipFlop bit0(.clk(clk), .reset(reset), .D(curr[0]), .q(next[0]), .qBar(0));
-	DFlipFlop bit1(.clk(clk), .reset(reset), .D(curr[1]), .q(next[1]), .qBar(0));
-	DFlipFlop bit2(.clk(clk), .reset(reset), .D(curr[2]), .q(next[2]), .qBar(0));
-	DFlipFlop bit3(.clk(clk), .reset(reset), .D(curr[3]), .q(next[3]), .qBar(0));
+	output [3:0] next;
 	
 	assign next[0] = ~curr[0];
 	assign next[1] = curr[0]^curr[1];
 	assign next[2] = curr[2]^(curr[1] & curr[0]);
 	assign next[3] = curr[3]^(curr[2] & curr[1] & curr[0]);
-
-	//assign count[0] = (in[0])^(in[1]);
-	//assign count[1] = (in[1])^(in[2]);
-	//assign count[2] = (in[2])^(in[3]);
-	//assign count[3] = (in[3]);
+	
+	DFlipFlop bit0(.clk(clk), .reset(reset), .D(curr[0]), .q(next[0]), .qBar(0));
+	DFlipFlop bit1(.clk(clk), .reset(reset), .D(curr[1]), .q(next[1]), .qBar(0));
+	DFlipFlop bit2(.clk(clk), .reset(reset), .D(curr[2]), .q(next[2]), .qBar(0));
+	DFlipFlop bit3(.clk(clk), .reset(reset), .D(curr[3]), .q(next[3]), .qBar(0));
 endmodule
 
 
